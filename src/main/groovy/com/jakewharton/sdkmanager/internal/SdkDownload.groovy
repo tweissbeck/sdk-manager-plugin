7ba4a422f129195ded23adfe65a16ed5b0d4e44e
Thierry Weissbeck <thierry.weissbeck@lyra-network.com>
2016-04-25 16:22:51 +0200

    Use proxy when downloading SDK

--- src/main/groovy/com/jakewharton/sdkmanager/internal/SdkDownload.groovy
+++ src/main/groovy/com/jakewharton/sdkmanager/internal/SdkDownload.groovy
@@ -5,75 +5,80 @@ import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 import org.rauschig.jarchivelib.ArchiverFactory
 
-import static com.android.SdkConstants.PLATFORM_DARWIN
-import static com.android.SdkConstants.PLATFORM_LINUX
-import static com.android.SdkConstants.PLATFORM_WINDOWS
-import static com.android.SdkConstants.currentPlatform
+import static com.android.SdkConstants.*
 import static org.rauschig.jarchivelib.ArchiveFormat.TAR
 import static org.rauschig.jarchivelib.ArchiveFormat.ZIP
 import static org.rauschig.jarchivelib.CompressionType.GZIP
 
 /** Manages platform-specific SDK downloads. */
 enum SdkDownload {
-  WINDOWS('windows','zip'),
-  LINUX('linux', 'tgz'),
-  DARWIN('macosx', 'zip');
+    WINDOWS('windows', 'zip'),
+    LINUX('linux', 'tgz'),
+    DARWIN('macosx', 'zip');
 
-  static SdkDownload get() {
-    switch (currentPlatform()) {
-      case PLATFORM_WINDOWS:
-        return WINDOWS
-      case PLATFORM_LINUX:
-        return LINUX
-      case PLATFORM_DARWIN:
-        return DARWIN
-      default:
-        throw new IllegalStateException("Unknown platform.")
-    }
-  }
-
-  final static SDK_VERSION = "24.4.1";
-
-  final Logger log = Logging.getLogger SdkDownload
-  final String suffix
-  final String ext
-
-  SdkDownload(String suffix, String ext) {
-    this.suffix = suffix
-    this.ext = ext
-  }
-
-  /** Download the SDK to {@code temp} and extract to {@code dest}. */
-  void download(File dest, String SDK_VERSION) {
-    def url = "http://dl.google.com/android/android-sdk_r$SDK_VERSION-$suffix.$ext"
-    log.debug "Downloading SDK from $url."
-
-    File temp = new File(dest.getParentFile(), 'android-sdk.temp')
-    temp.withOutputStream {
-      it << new URL(url).content
+    static SdkDownload get() {
+        switch (currentPlatform()) {
+            case PLATFORM_WINDOWS:
+                return WINDOWS
+            case PLATFORM_LINUX:
+                return LINUX
+            case PLATFORM_DARWIN:
+                return DARWIN
+            default:
+                throw new IllegalStateException("Unknown platform.")
+        }
     }
 
-    // Archives have a single child folder. Extract to the parent directory.
-    def parentFile = temp.getParentFile()
-    log.debug "Extracting SDK to $parentFile.absolutePath."
-    getArchiver().extract(temp, parentFile)
+    final static SDK_VERSION = "24.4.1";
 
-    // Move the aforementioned child folder to the real destination.
-    def extracted = new File(parentFile, "android-sdk-$suffix")
-    FileUtils.moveDirectory extracted, dest
+    final Logger log = Logging.getLogger SdkDownload
+    final String suffix
+    final String ext
 
-    // Delete downloaded archive.
-    temp.delete()
-  }
-
-  def getArchiver() {
-    switch (ext) {
-      case 'zip':
-        return ArchiverFactory.createArchiver(ZIP)
-      case 'tgz':
-        return ArchiverFactory.createArchiver(TAR, GZIP)
-      default:
-        throw new IllegalArgumentException("Unknown archive format '$ext'.")
+    SdkDownload(String suffix, String ext) {
+        this.suffix = suffix
+        this.ext = ext
     }
-  }
+
+    /** Download the SDK to {@code temp} and extract to {@code dest}. */
+    void download(File dest, String SDK_VERSION) {
+        def url = "http://dl.google.com/android/android-sdk_r$SDK_VERSION-$suffix.$ext"
+        log.debug "Downloading SDK from $url."
+
+        File temp = new File(dest.getParentFile(), 'android-sdk.temp')
+        temp.withOutputStream {
+            def proxyHost = java.lang.System.getProperty('http.proxyHost');
+            def proxyPort = java.lang.System.getProperty('http.proxyPort');
+            if (proxyHost != null && proxyPort != null) {
+                log.debug "Use proxy to downaload SDK: $proxyHost:$proxyPort"
+                def proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort.toInteger()))
+                it << new URL(url).openConnection(proxy).content
+            } else {
+                it << new URL(url).content
+            }
+        }
+
+        // Archives have a single child folder. Extract to the parent directory.
+        def parentFile = temp.getParentFile()
+        log.debug "Extracting SDK to $parentFile.absolutePath."
+        getArchiver().extract(temp, parentFile)
+
+        // Move the aforementioned child folder to the real destination.
+        def extracted = new File(parentFile, "android-sdk-$suffix")
+        FileUtils.moveDirectory extracted, dest
+
+        // Delete downloaded archive.
+        temp.delete()
+    }
+
+    def getArchiver() {
+        switch (ext) {
+            case 'zip':
+                return ArchiverFactory.createArchiver(ZIP)
+            case 'tgz':
+                return ArchiverFactory.createArchiver(TAR, GZIP)
+            default:
+                throw new IllegalArgumentException("Unknown archive format '$ext'.")
+        }
+    }
 }
